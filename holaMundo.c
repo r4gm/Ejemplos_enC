7de312c433d68a35270b17d1de46c4d792622012
r4gm <rafaglzmtz@gmail.com>
2021-08-19 18:12:14 -0500

    Nuevas Cosas

---
+++ holaMundo.c
@@ -0,0 +1,13 @@
+/*  Programa estructura para la estrutura
+    Nombre del Programa:
+    Autor:
+    Modificado por:
+
+*/
+#include <stdio.h>
+
+ int main(){
+
+     printf("Hello World\n");
+     
+ }
